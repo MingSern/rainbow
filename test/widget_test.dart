39525e93d72a0a0bd5ee655e0cc7e908ceed11b9
Yeo Ming Sern <winson.yeo@kskgroup.com>
2023-08-11 00:38:47 +0800

    master : Added check text test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:rainbow/apps/rainbow_app.dart';
+import 'package:rainbow/pages/home_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const RainbowApp());
+  testWidgets('Home page has a hello there text', (tester) async {
+    await tester.pumpWidget(const HomePage());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final textFinder = find.text('Hello there');
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(textFinder, findsOneWidget);
   });
 }
